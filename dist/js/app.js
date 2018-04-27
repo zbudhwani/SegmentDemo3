b7554581d5beda1aaa4653903a0286996015ab3e
Zishan Budhwani <zbudhwan@gmail.com>
2018-04-27 14:11:14 -0700

    6

--- dist/js/app.js
+++ dist/js/app.js
@@ -86,7 +86,7 @@ particlesJS('particles-js',
     "detect_on": "canvas",
     "events": {
       "onhover": {
-        "enable": false,
+        "enable": true,
         "mode": "repulse"
       },
       "onclick": {
